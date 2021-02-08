70afb928f062531097a09233dc42406dcf92de06
Rafael Arce Nazario <rafael.arce@upr.edu>
2021-02-08 09:53:00 -0400

    Arreglo a pruebas

--- main.cpp
+++ main.cpp
@@ -28,6 +28,20 @@ void moveZeroesLeft_v02( vector<int> &A ) {
 
 }
 
+
+// Dado un vector, un valor, un indice from y otro to, devolver
+// la cantidad de elementos entre los índices from y to que son
+// iguales al valor.
+
+int count(const vector<int> &A, int val, int from, int to) {
+    int ctr = 0;
+    for (int i = from; i <= to; i++) {
+        if (A[i] == val) ctr++;
+    }
+    return ctr;
+}
+
+
 int main() {
 	vector<int> A {10,20,0,30,40,0,0};
     vector<int> B {0,0,10,0,0,20,30,0,40,0,0};
@@ -35,13 +49,22 @@ int main() {
     vector<int> D {1,2,10,20};
 
     moveZeroesLeft_v01(A);
-    expect((A == vector<int>{10,20,30,40}));
+    // contar a ver si los tres ceros están al principio
+    expect(count(A,0,0,2) ==3);
+    // contar a ver si los tres ceros no están al final
+    expect(count(A,0,3,6) ==0);
+    // contar a ver si los hay un 10 en los elementos con indices del 3 al 6
+    expect(count(A,10,3,6)==1);
+    // contar a ver si los hay un 20 en los elementos con indices del 3 al 6
+    expect(count(A,20,3,6)==1);
 
     moveZeroesLeft_v01(B);
-    expect((B == vector<int>{10,20,30,40}));
+    // contar a ver si los tres ceros están al principio
+    expect(count(B,0,0,6) == 7);
+    expect(count(B,0,7,10) == 0);
 
     moveZeroesLeft_v01(C);
-    expect(C.size() == 0);
+    expect((C == vector<int>{0,0,0,0,0}));
 
     moveZeroesLeft_v01(D);
     expect((D == vector<int>{1,2,10,20}));
@@ -54,6 +77,5 @@ int main() {
 
 
     cout << "FIN! (si no ves Assertions arriba, ha pasado todas las pruebas)\n";
-
 
 }
